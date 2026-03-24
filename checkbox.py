ea1905de7d93c96d7734876759262bd4d925f05d
sakar kharel <97268953+sakarkharel@users.noreply.github.com>
2026-03-24 13:21:31 +0545

    feat: implement box checking using selenium

---
+++ checkbox.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+import time
+
+def main_page(driver):
+    form_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[@class='btn btn-lg'][normalize-space()='Checkbox']"))
+    )
+    form_button.click()
+    time.sleep(3)
+
+def checkbox(driver):
+    checkbox1 = driver.find_element(by=By.ID, value = "checkbox-1")
+    checkbox1.click()
+
+    checkbox2 = driver.find_element(by=By.ID, value= "checkbox-2")
+    checkbox2.click()
+
+    checkbox3 = driver.find_element(by=By.ID, value = "checkbox-3")
+    checkbox3.click()
+    time.sleep(3)
+    
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get("https://formy-project.herokuapp.com/")
+    driver.maximize_window()
+    main_page(driver)
+    checkbox(driver)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
+
+
